75e2f7723f0efe4830c6ac3861493fc713dc5d25
j0n9hyun <j0n9hyun@kakao.com>
2021-06-18 17:17:42 +0900

    Add Storybook

--- pages/posts/firstPost.tsx
+++ pages/posts/firstPost.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
-import styles from '../../styles/Home.module.scss';
-export default function firstPost() {
+// import styles from '../../styles/Home.module.scss';
+export default function firstPost({ primary = false, size = 'medium' }) {
   return (
     <>
       <Head>
         <title>페이지1</title>
       </Head>
-      <div className={styles.home}>
-        <Link href='../'>ㅁㄴㅇ</Link>
+      <div className=''>
+        <Link href='/'>ㅁㄴㅇ</Link>
       </div>
       <Image src='/images/jjong.png' width={150} height={50} alt='' />
     </>
